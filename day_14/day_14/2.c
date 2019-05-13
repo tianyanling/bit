f2b77c3c9e9f017ca42ccd070c9c3e4e571d86cb
tian <1157143244@qq.com>
2019-05-13 23:58:33 +0800

    提交代码

---
+++ day_14/day_14/2.c
@@ -0,0 +1,56 @@
+//2.判断一个字符串是否为另外一个字符串旋转之后的字符串。
+//例如：给定s1 = AABCD和s2 = BCDAA，返回1
+//给定s1 = abcd和s2 = ACBD，返回0.
+//AABCD左旋一个字符得到ABCDA
+//AABCD左旋两个字符得到BCDAA
+//AABCD右旋一个字符得到DAABC
+
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void leftturn(char *pstr1, int sz1, int k1)
+{
+	for (int i = 0; i < k1; i++)
+	{
+		char tmp = pstr1[0];		//第一个元素赋给temp
+		for (int j = 0; j < sz1 - 1; j++)
+		{
+			pstr1[j] = pstr1[j + 1];		//后面的元素一次向前移动一位
+		}
+		pstr1[sz1 - 1] = tmp;
+	}
+}
+
+void rightturn(char *pstr2, int sz2, int k2)
+{
+	for (int i = 0; i < k2; i++)
+	{
+		char tmp = pstr2[sz2-1];
+		for (int j = sz2-1; j >0; j--)
+		{
+			pstr2[j] = pstr2[j - 1];
+		}
+		pstr2[0] = tmp;
+	}
+}
+
+int main()
+{
+	char str1[] = "AABCD";
+	char str2[] = "AABCD";
+	int k1,k2;
+	printf("	请输入需要左旋字符的个数：");
+	scanf("%d", &k1);
+	printf("	请输入需要右旋字符的个数：");
+	scanf("%d", &k2);
+	int sz1 = sizeof(str1) / sizeof(str1[0]) - 1;
+	int sz2 = sizeof(str2) / sizeof(str2[0]) - 1;
+	leftturn(str1, sz1, k1);
+	rightturn(str2, sz2, k2);
+	printf("%s\n", str1);
+	printf("%s\n", str2);
+	system("pause");
+	return 0;
+}
