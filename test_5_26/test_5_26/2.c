d5d224c59bbb7a2018beb922c3862e59cfa273cf
tian <1157143244@qq.com>
2019-05-26 22:33:17 +0800

    提交代码

---
+++ test_5_26/test_5_26/2.c
@@ -0,0 +1,20 @@
+//2.Ä£ÄâÊµÏÖstrncat 
+
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<assert.h>
+
+char *my_strncat(char *dst, const char *src, size_t count)
+{
+
+}
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
