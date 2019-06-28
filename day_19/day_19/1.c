012af64bdaff5e9eb375e4f6a7f651e2d769bd5c
tian <1157143244@qq.com>
2019-06-28 22:22:03 +0800

    提交代码

--- day_19/day_19/1.c
+++ day_19/day_19/1.c
@@ -3,9 +3,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main()
+void print(int *p, int sz)
 {
-		
+	int i = 0;
+	for (i = 0; i<sz; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+	printf("\n");
+}
+
+int main1()
+{
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int *p = arr;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	print(p, sz);
 	system("pause");
 	return 0;
 }
--- day_19/day_19/2.c
+++ day_19/day_19/2.c
@@ -3,9 +3,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void test(int** ptr)
+{
+	printf("num = %d\n", **ptr);
+}
+
+
 int main()
 {
-
+	int n = 10;
+	int*p = &n;
+	int **pp = &p;
+	test(pp);
+	test(&p);
 	system("pause");
 	return 0;
 }
