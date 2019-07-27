4f0278c16f9b80990c70d200bec7e7b805929fcc
tian <1157143244@qq.com>
2019-07-27 21:55:15 +0800

    作业6

--- 作业/test_6/test_6/test1.c
+++ 作业/test_6/test_6/test1.c
@@ -3,8 +3,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int fun(int a, int b)
+{
+	if (a > b)
+		return (a + b);
+	else
+		return (a-b);
+}
+
 int main()
 {
+#if 0
+	int x = 3, y = 8, z = 6, r;
+	r = fun(fun(x, y), 2 * z);
+	printf("%d\n", r);
+#endif
 
 	system("pause");
 	return 0;
