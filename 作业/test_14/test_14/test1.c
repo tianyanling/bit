982bf2182677bec47cc1de984d890b4534bcd129
tian <1157143244@qq.com>
2019-08-15 19:19:53 +0800

    作业14

--- 作业/test_14/test_14/test1.c
+++ 作业/test_14/test_14/test1.c
@@ -3,9 +3,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#if 0
 int main()
 {
-
+	struct student
+	{
+		char name[10];
+		int age;
+		char gender;
+	}std[3],*p=std;
+	scanf("%d ", &(*p).age);
+	scanf("%c", &std[0].gender);
+	scanf("%c", &(p->gender));
+	scanf("%c",&std->name);
 	system("pause");
 	return 0;
 }
+#endif
