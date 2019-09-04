d72886615ea6cde67618b18c697442749fc0015d
tian <1157143244@qq.com>
2019-09-04 23:30:36 +0800

    作业18

---
+++ 作业/test_18/test_18/test1.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#if 0
+#define N 3
+#define Y(n) ((N+1)*n)
+
+int main()
+{
+	int z = 2 * (N + Y(5 + 1));
+	printf("%d\n", z);
+	system("pause");
+	return 0;
+}
+#endif
+
+#if 0
+int main()
+{
+	int i = 6;
+	if (i <= 6)
+	{
+		printf("hello\n");
+	}
+	else
+	{
+		printf("bye-bye\n");
+	}
+	system("pause");
+	return 0;
+}
+#endif
+
+#if 0
+int main()
+{
+	char ch = -1;
+	printf("%d\n", ch);
+	system("pause");
+	return 0;
+}
+#endif
