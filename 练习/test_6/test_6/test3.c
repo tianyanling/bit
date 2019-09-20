6cc4c641ff936a91b7aeb70ee1f1bf22aa3c2569
tian <1157143244@qq.com>
2019-09-20 23:06:19 +0800

    代码练习

---
+++ 练习/test_6/test_6/test3.c
@@ -0,0 +1,14 @@
+//要求逆置单链表（不带头结点）
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+	char s[] = "\\123456\123456\t";
+	printf("%d\n", strlen(s));
+	system("pause");
+	return 0;
+}
