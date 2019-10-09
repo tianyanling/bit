cfd41004cb90a32c01f2a4f7526355370439ed4c
tian <1157143244@qq.com>
2019-10-09 23:08:01 +0800

    两个栈实现队列

---
+++ C++/10_08/10_08/test1.cpp
@@ -0,0 +1,71 @@
+//两个栈实现队列
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+
+void change(stack<int> *s1, stack<int> *s2)
+{
+	int i;
+	for (i = 0; i < s1->size(); i++)
+	{
+		s2->push(s1->top());
+		s1->pop();
+	}
+}
+class MyQueue
+{
+	stack<int> *obj;
+public:
+	MyQueue() 
+	{
+		obj = new stack<int>;
+	}
+
+	void push(int x) 
+	{
+		obj->push(x);
+	}
+
+	int pop()
+	{
+		stack<int> *tmp = new stack<int>;
+		change(obj, tmp);
+		int p = tmp->top();
+		tmp->pop();
+		change(tmp, obj);
+		return p;
+	}
+
+	int peek() 
+	{
+		stack<int> *tmp = new stack<int>;
+		change(obj, tmp);
+		int p = tmp->top();
+		change(tmp, obj);
+		return p;
+	}
+
+	bool empty()
+	{
+		return obj->empty();
+	}
+};
+
+int main1()
+{
+	queue<int> q;
+	q.push(2);
+	q.push(4);
+	q.push(6);
+	cout << q.front() << endl;
+
+	q.pop();
+	cout << q.front() << endl;
+
+	q.pop();
+	cout << q.front() << endl;
+
+	system("pause"); 
+	return 0;
+}
