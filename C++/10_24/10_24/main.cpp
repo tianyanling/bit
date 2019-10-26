801adcffe7fe0d89863206b1ba77799172975a64
tian <1157143244@qq.com>
2019-10-26 23:10:09 +0800

    提交代码

---
+++ C++/10_24/10_24/main.cpp
@@ -0,0 +1,17 @@
+//È±Ê¡²ÎÊý
+#include<iostream>
+using namespace std;
+
+void TestFunc(int a, int b = 10, int c = 20)
+{
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl;
+}
+
+int main()
+{
+	TestFunc(10);
+	system("pause");
+	return 0;
+}
