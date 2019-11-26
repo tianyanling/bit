3598895aae40fb47277c02a8aa12bb2f60f20ab9
tian <1157143244@qq.com>
2019-11-26 22:30:41 +0800

    AVL树

---
+++ C++/11_26/11_26/main.cpp
@@ -0,0 +1,29 @@
+#include"AVLTree.h"
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+	tdd::AVLTree<int> bst;
+
+	//bst.insert(5);
+	//bst.insert(2);
+	//bst.insert(8);
+	//bst.insert(6);
+	//bst.insert(4);
+	//bst.insert(7);
+	//bst.insert(1);
+
+	bst.insert(1);
+	bst.insert(2); 
+	bst.insert(3);
+	bst.insert(4);
+	bst.insert(5);
+	bst.insert(6);
+	bst.insert(7);
+
+
+	system("pause");
+	return 0;
+}
