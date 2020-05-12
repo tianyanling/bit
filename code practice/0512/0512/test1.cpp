4349fbda94bff5be9294093fcb1b158f908feac4
tian <1157143244@qq.com>
2020-05-12 23:54:30 +0800

    练习题

---
+++ code practice/0512/0512/test1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unordered_set>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int lengthOfLongestSubstring(string s)
+{
+	if (s.size() == 0)
+		return 0;
+
+	unordered_set<char> lookup;
+	int maxStr = 0;
+	int left = 0;
+	for (int i = 0; i < s.size(); i++)
+	{
+		while (lookup.find(s[i]) != lookup.end())
+		{
+			lookup.erase(s[left]);
+			left++;
+		}
+		maxStr = max(maxStr, i - left + 1);
+		lookup.insert(s[i]);
+	}
+	return maxStr;
+}
+
+int main()
+{
+	string s = "abcabcbb";
+	cout << lengthOfLongestSubstring(s) << endl;
+	system("pause");
+	return 0;
+}
